4d8fbd2799bec714ec6480dcfc4fc8213caa029f
Meaghan Neylon <mneylon@richmond.edu>
2020-09-08 16:42:05 -0400

    Added alert and prompt to allow the user to select a password length.

--- script.js
+++ script.js
@@ -10,6 +10,29 @@ function writePassword() {
 
 }
 
+//Generate Password for Password Generator. 
+
+function generatePassword() {
+  console.log("Generate Password Here"); // make sure the page is working. 
+
+  //Set up the length for the password:
+
+  var length = Number (
+    prompt("How long would you like your password to be? Please choose between 8-128 characters.")
+  ); 
+
+  //See how many characters the user would like to have:
+  console.log("The user chose: " + length);
+
+  //Create a min/max password length. 
+  if (length <8 || length >128) {
+    alert ("Please choose a password length between 8-128");
+    return;
+  }
+
+  
+}
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
