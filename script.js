f8ad08616438849905846015c16260891a4a0c3a
Meaghan Neylon <mneylon@richmond.edu>
2020-09-08 16:44:39 -0400

    Added character choice arrays

--- script.js
+++ script.js
@@ -30,7 +30,16 @@ function generatePassword() {
     return;
   }
 
-  
+  //possible characters in array format. 
+  var numerical = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]; 
+  var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+"];
+  var chosenChar = []; //Array for the users selected characters. 
+
+  //Confirms asking the user what type of characters they would like to use. 
+
+
 }
 
 
