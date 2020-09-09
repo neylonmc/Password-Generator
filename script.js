f1edaa5da6c468d730f070b853c22fa6530f0784
Meaghan Neylon <mneylon@richmond.edu>
2020-09-08 21:27:54 -0400

    Updated commit.

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+  
 
 }
 
@@ -31,7 +33,7 @@ function generatePassword() {
   }
 
   //possible characters in array format. 
-  var numerical = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  var numerical = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
   var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]; 
   var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+"];
@@ -44,11 +46,12 @@ function generatePassword() {
   var useSpecialChar = confirm("Would you like to have any special characters in your password? Example: !@#$%^&*()_+");
 
   //Check their answers..
-  console.log ( {useNumerical, useLowerCase, useUpperCase, useSpecialChar});
+  console.log (useNumerical, useLowerCase, useUpperCase, useSpecialChar );
 
   //Create arrays based on their answers using .push: 
 
-  if (useNumerical) {
+
+ if (useNumerical) {
   chosenChar.push(numerical);
   }
 
@@ -90,17 +93,10 @@ function generatePassword() {
     console.log(passwordChosen + " is the chosen password from this array"); //selects character from the chosen index number. 
 
     password += passwordChosen //password
-
   
   }
   return password;
-
-  
 } 
-
-
-
-
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword)
